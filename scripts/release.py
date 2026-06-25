988cd9445ee2adef3a043ad52e253104c477d084
Teknium <127238744+teknium1@users.noreply.github.com>
2026-06-25 11:00:26 -0700

    chore: add kuangmi-bit to AUTHOR_MAP for salvaged P0 fixes

--- scripts/release.py
+++ scripts/release.py
@@ -749,6 +749,7 @@ AUTHOR_MAP = {
     "252620095+briandevans@users.noreply.github.com": "briandevans",
     "incharge.automation@gmail.com": "inchargeautomation-lab",
     "danielrpike9@gmail.com": "Bartok9",
+    "kuangmi@deeparchi.com": "kuangmi-bit",
     "96944678+ymylive@users.noreply.github.com": "sweetcornna",
     "laflamme@illinoisalumni.org": "briancl2",
     "skozyuk@cruxexperts.com": "CruxExperts",
